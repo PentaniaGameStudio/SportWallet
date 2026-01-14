c0469046b7aa7fbc145ab6c3dfb9442515cb6f13
PentaniaGameStudio <102145837+PentaniaGameStudio@users.noreply.github.com>
2026-01-14 07:13:00 +0100

    Keep Screen On setting. To remove later : NavHost ligne 29 "KeepScreenOn(enabled = true)"

--- app/src/main/java/com/sportwallet/core/util/KeepScreenOn.kt
+++ app/src/main/java/com/sportwallet/core/util/KeepScreenOn.kt
@@ -2,9 +2,7 @@ package com.sportwallet.ui.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import android.view.WindowManager
 
 @Composable
 fun KeepScreenOn(enabled: Boolean) {
